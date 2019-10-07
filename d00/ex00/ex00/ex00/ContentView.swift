11cf2e81376549a70d9c0077b70c5eeea45eadc7
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-10-07 12:42:04 +0200

    Setting up ex00

---
+++ d00/ex00/ex00/ex00/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ex00
+//
+//  Created by Samantha HILLEBRAND on 2019/10/07.
+//  Copyright © 2019 Samantha HILLEBRAND. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
