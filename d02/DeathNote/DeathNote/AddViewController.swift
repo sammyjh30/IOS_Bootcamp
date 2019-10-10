0b788355e8f8f2bdfb979bdffde9d921cee54cf1
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-10-10 17:51:00 +0200

    Slowly getting there

---
+++ d02/DeathNote/DeathNote/AddViewController.swift
@@ -0,0 +1,46 @@
+//
+//  AddViewController.swift
+//  DeathNote
+//
+//  Created by Samantha HILLEBRAND on 2019/10/10.
+//  Copyright © 2019 Samantha HILLEBRAND. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    @IBAction func doneButton(_ sender: Any) {
+        print (nameTextField.text!)
+        print (textDescription.text!)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd MMM YYYY HH:mm"
+        let somedateString = dateFormatter.string(from: datePicker.date)
+
+        print (somedateString)
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var textDescription: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let currentDate = Date()
+        datePicker.minimumDate = currentDate
+        descriptionTextView!.layer.borderWidth = 1
+        descriptionTextView!.layer.borderColor = UIColor.lightGray.cgColor
+    }
+
+    //Hides keyboard when done
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
+    
+
+    
+}
