7f0772aa8afef4ba0be3c722f3e4de3e0b250a89
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-10-11 17:15:56 +0200

    Setting up for ex01

---
+++ d03/Pictures/Pictures/CellClass.swift
@@ -0,0 +1,14 @@
+//
+//  CellClass.swift
+//  Pictures
+//
+//  Created by Samantha HILLEBRAND on 2019/10/11.
+//  Copyright © 2019 Samantha HILLEBRAND. All rights reserved.
+//
+
+import UIKit
+
+class CellClass: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
