f25bf01fb73d13b15b849b881c531429973ed142
Samantha HILLEBRAND <shillebr@c1r2s3.wethinkcode.co.za>
2019-10-12 19:45:29 +0200

    Done ex01

---
+++ d04/Tweeter/Tweeter/Controller/APITwitterDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  APITwitterDelegate.swift
+//  Tweeter
+//
+//  Created by Samantha HILLEBRAND on 2019/10/12.
+//  Copyright © 2019 Samantha Hillebrand. All rights reserved.
+//
+
+import Foundation
+
+protocol APITwitterDelegate {
+    //https://docs.swift.org/swift-book/LanguageGuide/Protocols.html
+    func listener(tweet: [Tweet])
+    func NSExceptionErr(err: NSError)
+}
