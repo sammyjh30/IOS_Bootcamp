b8c71233c203c55199e206518d6b3783f95b3175
sammyjh30 <36789037+sammyjh30@users.noreply.github.com>
2019-10-14 11:38:26 +0200

    Setting up day 05. Done ex00

---
+++ d05/Kanto/Kanto/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Kanto
+//
+//  Created by Samantha HILLEBRAND on 2019/10/14.
+//  Copyright © 2019 Samantha Hillebrand. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ d05/Kanto/Kanto/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Kanto
+//
+//  Created by Samantha HILLEBRAND on 2019/10/14.
+//  Copyright © 2019 Samantha Hillebrand. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
