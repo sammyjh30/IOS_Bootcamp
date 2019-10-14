6b1bc9d9b8ab63f53cc48f430c6381347300dd2a
sammyjh30 <36789037+sammyjh30@users.noreply.github.com>
2019-10-14 13:27:35 +0200

    done ex01

--- d05/Kanto/Kanto/FirstViewController.swift
+++ d05/Kanto/Kanto/FirstViewController.swift
@@ -8,11 +8,37 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let transportItems = ["WeThinkCode_","Waterfront","Canal Walk","Seapoint","Camps Bay","Howard Centre"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    //the method returning size of the list
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return transportItems.count
+    }
+
+    //the method returning each cell of the list
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        //displaying values
+        cell.textLabel?.text = transportItems[indexPath.row]
+        return cell
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
