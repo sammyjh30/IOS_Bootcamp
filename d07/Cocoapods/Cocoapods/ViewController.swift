17120a2714e5360013fcdbe562db6c31e0e84fa9
sammyjh30 <36789037+sammyjh30@users.noreply.github.com>
2019-10-17 19:09:18 +0200

    Trying to get call to work

--- d07/Cocoapods/Cocoapods/ViewController.swift
+++ d07/Cocoapods/Cocoapods/ViewController.swift
@@ -10,29 +10,56 @@ import UIKit
 //import ForecastIO
 import RecastAI
 
-let forecastToken: String = "My_darksky_token"
-let recastToken: String = "My_recast_token"
+let recastToken: String = "0e12962c8287d8b6d6326872d010faf9"
 
 class ViewController: UIViewController {
-
+    
     var chatBot: RecastAIClient?
-//    var forecastClient: APIClient?
-    var nbrRequest: Int? = nil
-    //Not sure what that is /\
-//    var locations: [Location] = []      //Keep track of locations?
     
     @IBOutlet weak var botTextField: UITextField!
     @IBOutlet weak var botLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        self.bot = RecastAIClient(token: recastToken, handlerRecastRequestProtocol: self)
-        
-        if nbrRequest == nil {
-            
+        self.chatBot = RecastAIClient(token: recastToken, language: "en")
+        print("Loaded")
+    }
+    @IBAction func onSendButtonPress(_ sender: Any) {
+        if botTextField.text != "" {
+            makeRequest(input: botTextField.text!)
+        } else {
+            botLabel.text = "Bad input :("
         }
     }
-
-
+    
+    func makeRequest(input: String) {
+        self.chatBot?.textRequest(input, successHandler: recastRequestDone(_:), failureHandle: recastRequestFailed(_:))
+    }
+    
+    func recastRequestDone(_ response : Response)
+    {
+        var toPrint: String = ""
+        let datetime = response.get(entity: "datetime")
+        let location = response.get(entity: "location")
+        print("Stuff \(response)")
+        if (datetime != nil) {
+            let formatted = datetime!["formatted"] as! String
+            toPrint += "\(formatted)"
+        }
+        if (location != nil) {
+            toPrint += "\n\(location!["city"] as! String)"
+        }
+        if toPrint == "" {
+            toPrint = "No data available"
+        }
+        botLabel.text = toPrint
+    }
+    
+    func recastRequestFailed(_ error: Error)
+    {
+        botLabel.text = "Error"
+        
+        print("Failed \(error)")
+    }
 }
 
